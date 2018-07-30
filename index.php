6ef3f4ae9f8fd42b826c2b732c36ddabde378014
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-30 15:49:51 +0200

    exo4p6 php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+<?php
+  echo $_GET['language']
+?>
+<br />
+<?php
+  echo $_GET['server'];
+?>
+    </p>
+  </body>
+</html>
